c2f726a7e8cca6db6496e33c49a54f9976d58f4e
Edvinas Aleksejonokas <Edvinas.Aleksejonokas@oxid-esales.com>
2016-12-08 12:44:55 +0200

    ESDEV-4156 Simplify getter for view object

--- source/Setup/Controller.php
+++ source/Setup/Controller.php
@@ -40,6 +40,14 @@ class Controller extends Core
     // BEGIN: Controllers
 
     /**
+     * Controller constructor
+     */
+    public function __construct()
+    {
+        $this->view = new View();
+    }
+
+    /**
      * First page with system requirements check
      *
      * @return string
@@ -484,10 +492,6 @@ class Controller extends Core
      */
     public function getView()
     {
-        if ($this->view == null) {
-            $this->view = new View();
-        }
-
         return $this->view;
     }
 
